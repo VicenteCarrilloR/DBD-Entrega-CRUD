f6ad342658425cd483cb2a32899dbf1f42ff39b3
CarPaz <carla.astroza@usach.cl>
2025-07-07 19:35:30 -0400

    Carta, juego de mesa y producto

---
+++ src/main/java/entity/Carta.java
@@ -0,0 +1,89 @@
+package entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "carta")
+public class Carta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_carta")
+    private Integer idCarta;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", referencedColumnName = "id_producto")
+    private Producto producto;
+
+    @Column(name = "nombre_carta", length = 40)
+    private String nombreCarta;
+
+    @Column(name = "descripcion", length = 500)
+    private String descripcion;
+
+    @Column(name = "año")
+    private LocalDate año;
+
+    @Column(name = "estado", length = 40)
+    private String estado;
+
+    @Column(name = "tipo_carta", length = 40)
+    private String tipoCarta;
+
+    public Integer getIdCarta() {
+        return idCarta;
+    }
+
+    public void setIdCarta(Integer idCarta) {
+        this.idCarta = idCarta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public String getNombreCarta() {
+        return nombreCarta;
+    }
+
+    public void setNombreCarta(String nombreCarta) {
+        this.nombreCarta = nombreCarta;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDate getAño() {
+        return año;
+    }
+
+    public void setAño(LocalDate año) {
+        this.año = año;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTipoCarta() {
+        return tipoCarta;
+    }
+
+    public void setTipoCarta(String tipoCarta) {
+        this.tipoCarta = tipoCarta;
+    }
+
+}
---
+++ src/main/java/entity/Producto.java
@@ -0,0 +1,65 @@
+package entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="producto")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    @ManyToOne
+    @JoinColumn(name = "id_tienda", referencedColumnName = "id_tienda")
+    private Tienda tienda;
+
+    @Column(name = "precio")
+    private Integer precio;
+
+    @Column(name = "url_imagen", length = 100)
+    private String urlImagen;
+
+    @Column(name = "stock")
+    private Integer stock;
+
+    public Integer getIdProducto(){
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto){
+        this.idProducto = idProducto;
+    }
+
+    public Tienda getTienda() {
+        return tienda;
+    }
+
+    public void setTienda(Tienda tienda) {
+        this.tienda = tienda;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    public String getUrlImagen() {
+        return urlImagen;
+    }
+
+    public void setUrlImagen(String urlImagen) {
+        this.urlImagen = urlImagen;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+}
