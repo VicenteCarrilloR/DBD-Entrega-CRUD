bf8063dcc201a905ddaa1ad8af52548d93d121e4
CarPaz <carla.astroza@usach.cl>
2025-07-08 18:46:52 -0400

    Repositorios faltantes

---
+++ src/main/java/com/example/demo/repository/MedioDePagoRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.MedioDePago;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedioDePagoRepository extends JpaRepository<MedioDePago, Integer>{
+}
---
+++ src/main/java/com/example/demo/repository/RolRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+}
