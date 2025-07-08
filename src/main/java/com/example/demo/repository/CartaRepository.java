ac336b3a382f7f9b954adbc0a5913d42ca463b24
CarPaz <carla.astroza@usach.cl>
2025-07-07 21:10:48 -0400

    repositorios Carta, juego de mesa y producto

--- src/main/java/com/example/demo/repository/CartaRepository.java
+++ src/main/java/com/example/demo/repository/CartaRepository.java
@@ -4,5 +4,5 @@ import com.example.demo.entity.Carta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface CartaRepository extends JpaRepository<Carta,Integer>{
+public interface CartaRepository extends JpaRepository<Carta, Integer>{
 }
--- src/main/java/com/example/demo/repository/ProductoRepository.java
+++ src/main/java/com/example/demo/repository/ProductoRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductoRepository extends JpaRepository<Producto, Integer>{
+public interface ProductoRepository extends JpaRepository<Producto,Integer>{
 }
